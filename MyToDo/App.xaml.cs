5012a91a2c41fa0b6f0d81f182a6a1ebe0e75d54
FAll <940313262@qq.com>
2023-11-29 18:30:31 +0800

    添加项目文件。

---
+++ MyToDo/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.DryIoc;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MyToDo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+
+}
---
+++ MyToDo/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace MyToDo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /* 初始化窗口事件 */
+            // 最小化
+            btnMin.Click += (s, e) => { WindowState = WindowState.Minimized; };
+            // 最大化
+            btnMax.Click += (s, e) =>
+            {
+                WindowState = WindowState == WindowState.Maximized 
+                ? WindowState.Normal 
+                : WindowState.Maximized;
+            };
+            // 关闭窗口
+            btnClose.Click += (s, e) => { Close(); };
+            // 拖动窗口
+            ColorZone.MouseMove += (s, e) =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    this.DragMove();
+            };
+        }
+    }
+}
